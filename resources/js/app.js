87527d381720779c930984e790590f471358fa2e
Patrick Jerome Uy <83640317+patrickjeromeuy@users.noreply.github.com>
2022-06-16 13:36:11 +0800

    Added validations for contact us page

--- resources/js/app.js
+++ resources/js/app.js
@@ -29,3 +29,30 @@ window.addEventListener('DOMContentLoaded', event => {
     }
 
 });
+
+$('#contactus-btn').on('click', function () {
+    let name = $('#name').val();
+    let email = $('#email').val();
+    let subject = $('#subject').val();
+    let message = $('#message').val();
+
+    var mail_format = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (name == '' || email == '' || subject == '' || message == '') {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error!',
+          text: 'Please fill up the fields!',
+          confirmButtonColor: '#2a2f89',
+        })
+          return false;
+    } else if (!(email.match(mail_format))) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error!',
+          text: 'Please enter a valid email',
+          confirmButtonColor: '#2a2f89',
+        })
+        return false;
+    }
+});
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -184,6 +184,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
+
+    <!-- Sweet Alert 2 -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.4.17/dist/sweetalert2.all.min.js"></script>
 </body>
 
 </html>
