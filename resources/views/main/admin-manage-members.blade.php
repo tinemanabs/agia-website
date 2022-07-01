64eb0d81b714f548c90142dfa1a0f58c087a02aa
tinemanabs <christinemmanabat@gmail.com>
2022-07-01 12:26:27 +0800

    Update admin-manage-members.blade.php

--- resources/views/main/admin-manage-members.blade.php
+++ resources/views/main/admin-manage-members.blade.php
@@ -5,10 +5,11 @@
         <div class="card-body">
             <div class="d-flex justify-content-between align-items-center">
                 <h5>List of Registered Members</h5>
-                <a class="btn btn-primary" href="{{ route('admin.register-member') }}" role="button"><i
-                        class="fa-solid fa-plus"></i></a>
+                {{-- <a class="btn btn-primary" href="{{ route('admin.register-member') }}" role="button"><i
+                        class="fa-solid fa-plus"></i></a> --}}
             </div>
-            <button class="btn btn-danger" id="multi-delete" data-route="{{ route('multi-delete.member') }}">Delete All Selected</button>
+            <button class="btn btn-danger" id="multi-delete" data-route="{{ route('multi-delete.member') }}">Delete All
+                Selected</button>
             <div class="table-responsive">
                 <table class="stripe" id="myTable">
                     <thead>
@@ -28,27 +29,29 @@
                                 <th>{{ $user->id }}</th>
                                 <td>{{ $user->name }}</td>
                                 <td>{{ $user->email }}</td>
-                                <td>{{ '@' .  $user->username }}</td>
-                                <td><button type="button" class="btn btn-circle btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#id{{ $user->id }}">
+                                <td>{{ '@' . $user->username }}</td>
+                                <td><button type="button" class="btn btn-circle btn-sm btn-primary" data-bs-toggle="modal"
+                                        data-bs-target="#id{{ $user->id }}">
                                         <i class="fa-solid fa-search"></i>
                                     </button>
 
-                                <form style="display: inline;" action="{{ route('delete.member') }}" method="post">
-                                    @csrf
-                                    <input type="hidden" name="id" id="id" value="{{ $user->id }}">
-                                    <button class="btn btn-circle btn-sm btn-danger" type="submit">
-                                        <i class="fa-solid fa-trash"></i>
-                                    </button>
-                                </form>
+                                    <form style="display: inline;" action="{{ route('delete.member') }}" method="post">
+                                        @csrf
+                                        <input type="hidden" name="id" id="id" value="{{ $user->id }}">
+                                        <button class="btn btn-circle btn-sm btn-danger" type="submit">
+                                            <i class="fa-solid fa-trash"></i>
+                                        </button>
+                                    </form>
 
                                 </td>
                             </tr>
                             <!-- Modal -->
                             <div class="modal fade" id="id{{ $user->id }}" tabindex="-1" aria-hidden="true">
-                                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl">
                                     <div class="modal-content">
                                         <div class="modal-header">
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
                                         </div>
                                         <div class="modal-body">
                                             <div class="container">
@@ -66,7 +69,7 @@
                                                 </div>
                                                 <div class="row mb-3">
                                                     <div class="col-3"> <strong>Username</strong></div>
-                                                    <div class="col-9">{{ '@' .  $user->username }}</div>
+                                                    <div class="col-9">{{ '@' . $user->username }}</div>
                                                 </div>
                                                 <div class="row mb-3">
                                                     <div class="col-9"> <strong>Membership Application</strong></div>
@@ -75,7 +78,8 @@
                                             </div>
                                         </div>
                                         <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                            <button type="button" class="btn btn-secondary"
+                                                data-bs-dismiss="modal">Close</button>
                                         </div>
                                     </div>
                                 </div>
